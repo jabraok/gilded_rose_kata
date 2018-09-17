4aacc17ef08cf9ebe70b60776c00f4d360be3121
Vinh Luu <vinhlt3@gmail.com>
2018-09-17 12:44:41 +0700

    Refactory for Sulfuras

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,10 +1,10 @@
 def update_quality(items)
   items.each do |item|
+    return item if item.name == 'Sulfuras, Hand of Ragnaros'
+
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
+        item.quality -= 1
       end
     else
       if item.quality < 50
@@ -23,16 +23,14 @@ def update_quality(items)
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
+
+    item.sell_in -= 1
+
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
+            item.quality -= 1
           end
         else
           item.quality = item.quality - item.quality

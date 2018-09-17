7103973f11830b3247af62be1f56708c9816e150
Vinh Luu <vinhlt3@gmail.com>
2018-09-17 12:57:03 +0700

    Create update_backstage method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,26 +1,37 @@
+def update_backstage(item)
+  if item.quality < 50
+    item.quality += 1
+    if item.sell_in < 11
+      if item.quality < 50
+        item.quality += 1
+      end
+    end
+    if item.sell_in < 6
+      if item.quality < 50
+        item.quality += 1
+      end
+    end
+  end
+
+  item.sell_in -= 1
+
+  if item.sell_in < 0
+    item.quality = item.quality - item.quality
+  end
+end
+
 def update_quality(items)
   items.each do |item|
     return item if item.name == 'Sulfuras, Hand of Ragnaros'
+    return update_backstage(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.name != 'Aged Brie'
       if item.quality > 0
         item.quality -= 1
       end
     else
       if item.quality < 50
         item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
       end
     end
 
@@ -28,12 +39,8 @@ def update_quality(items)
 
     if item.sell_in < 0
       if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality -= 1
-          end
-        else
-          item.quality = item.quality - item.quality
+        if item.quality > 0
+          item.quality -= 1
         end
       else
         if item.quality < 50

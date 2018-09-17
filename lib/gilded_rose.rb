3d5b05b3d47973b124abd54d53140bb33817039a
Vinh Luu <vinhlt3@gmail.com>
2018-09-17 12:59:27 +0700

    Add update_aged_brie method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -20,32 +20,35 @@ def update_backstage(item)
   end
 end
 
+def update_aged_brie(item)
+  if item.quality < 50
+    item.quality += 1
+  end
+
+  item.sell_in -= 1
+
+  if item.sell_in < 0
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+end
+
 def update_quality(items)
   items.each do |item|
     return item if item.name == 'Sulfuras, Hand of Ragnaros'
     return update_backstage(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    return update_aged_brie(item) if item.name == 'Aged Brie'
 
-    if item.name != 'Aged Brie'
-      if item.quality > 0
-        item.quality -= 1
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-      end
+    if item.quality > 0
+      item.quality -= 1
     end
 
     item.sell_in -= 1
 
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.quality > 0
-          item.quality -= 1
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      if item.quality > 0
+        item.quality -= 1
       end
     end
   end
